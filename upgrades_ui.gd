e1016bc7504c737248cf47f66b9da4d3cacf37b3
wdjakeae <ttvwdjake@gmail.com>
2025-01-31 18:33:49 +0100

    inodwanodainjido

---
+++ upgrades_ui.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@export var player: Player 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_dmg_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_hp_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_def_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_atkspeed_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_mvmntspeed_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stsdur_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stsdmg_pressed() -> void:
+	pass # Replace with function body.
