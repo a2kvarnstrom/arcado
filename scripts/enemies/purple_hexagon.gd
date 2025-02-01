dae29740d1c03e4b00721ffb83cbf5e5c5ae4cc6
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2025-02-01 18:13:38 +0100

    d

--- scripts/enemies/purple_hexagon.gd
+++ scripts/enemies/purple_hexagon.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+<<<<<<< Updated upstream
 
 @export var SPEED = 50.0
 
@@ -10,3 +11,31 @@ func _draw() -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
+=======
+@export var SPEED = 50.0
+@onready var effect_range: Area2D = $EffectRange
+var enemy_affected: CharacterBody2D
+
+func _draw() -> void:
+	draw_circle(Vector2(0.0, -15), 30, Color.PURPLE, true, -1.0, true)
+	draw_circle(Vector2(0.0, 15), 30, Color.PURPLE, true, -1.0, true)
+	draw_rect(Rect2(-30.5, -20, 61, 40), Color.PURPLE, true, -1.0, true)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_effect_range_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print_debug("body enter :D")
+	if(!enemy_affected):
+		if(!body.has_method("pause") && body.has_node("Health")):
+			enemy_affected = body
+			enemy_affected.get_node("./Hurtbox").damage_reduction = 100
+			print(enemy_affected.get_node("./Hurtbox").damage_reduction)
+
+
+func _on_effect_range_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print_debug("body exit D:")
+	if(body == enemy_affected):
+		enemy_affected.get_node("./Hurtbox").damage_reduction = 0.0
+		enemy_affected = null
+>>>>>>> Stashed changes
--- scripts/systems/entity/hurtbox.gd
+++ scripts/systems/entity/hurtbox.gd
@@ -6,6 +6,7 @@ signal received_damage(damage: float)
 @onready var hitbox: Hitbox
 @export var health: Health
 @export var pierce: int = 1
+@export var damage_reduction: float = 0.0
 
 func _ready() -> void:
 	connect("area_entered", _on_area_entered)
@@ -13,7 +14,7 @@ func _ready() -> void:
 func _on_area_entered(hit_box: Area2D) -> void:
 	if(hit_box != null):
 		if(hit_box is Hitbox):
-			health.set_health(health.health - hit_box.damage)
+			health.set_health(health.health - (hit_box.damage * ((100-damage_reduction)/100)))
 			received_damage.emit(hit_box.damage)
 			if(hit_box.pierce):
 				hit_box.pierce.set_pierce(hit_box.pierce.get_pierce() - pierce)
